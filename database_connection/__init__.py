f94c2f90d868803e658db0e17bfd7a6f1bd69cf3
willcollierADL <96437133+willcollierADL@users.noreply.github.com>
2022-06-27 14:53:46 +0100

    add api queries for google api, added to requirements and init to make it work

--- database_connection/__init__.py
+++ database_connection/__init__.py
@@ -1,2 +1,3 @@
 from .sql_connection import get_connection, run_sql, run_sql_text_query, row_to_df, \
     drop_table, delete_records, turn_data_into_insert
+from .google_api_query import ga_api_caller, pivot_report_df, metric_report_df, get_dimension_row_names, run_report
